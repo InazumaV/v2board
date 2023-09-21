6fc15f5885221284de88b6d243ad1d43b41447d3
AiOhto <84896599+linger040831@users.noreply.github.com>
2023-09-21 09:07:13 +0800

    Update update.sql
    fix:v2_server_vmess

--- database/update.sql
+++ database/update.sql
@@ -630,7 +630,9 @@ ALTER TABLE `v2_server_v2ray`
 ALTER TABLE `v2_server_vmess`
     CHANGE `network` `network` varchar(11) COLLATE 'utf8mb4_general_ci' NOT NULL AFTER `rate`;
     CHANGE `networkSettings` `network_settings` TEXT,
-    CHANGE `tlsSettings` `tls_settings` TEXT,
+    CHANGE `tlsSettings` `tls_settings` TEXT;
+    
+ALTER TABLE `v2_server_vmess`
     DROP `ruleSettings`,
     DROP `dnsSettings`;
 
